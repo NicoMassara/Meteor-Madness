ad252c65cfeabb56f5dbdf0bffe9a3372027b3d2
NicoMassara <nicolasmassara@outlook.com>
2025-10-31 12:49:38 -0300

    Selector Added and working

---
+++ Assets/_Main/Scripts/Tutorial/TutorialUiSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _Main.Scripts.Interfaces;
+using _Main.Scripts.Utilities;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Main.Scripts.Tutorial
+{
+    public class TutorialUiSelector : UiPanelSelector<TutorialUiComponents> { }
+    
+    [Serializable]
+    public class TutorialUiComponents : UiComponentsData
+    {
+        [Space(2)] 
+        [Header("Sub Panels")] 
+        public GameObject StartPanel;
+        public GameObject HintPanel;
+        [Space]
+        [Header("Buttons Components")]
+        public Button StartButton;
+        public Button MainMenuButton;
+        [Space]
+        [Header("Text Components")]
+        public TMP_Text HintText;
+    }
+}
---
+++ Assets/_Main/Scripts/Utilities/UiPanelSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using _Main.Scripts.Interfaces;
+using UnityEngine;
+
+namespace _Main.Scripts.Utilities
+{
+    public abstract class UiPanelSelector<T> : MonoBehaviour, IPanelSelector<T>
+    where T : UiComponentsData
+    {
+        [SerializeField] private string panelName;
+        [Space(5)]
+        [SerializeField] private T mobileData;
+        [Space(5)]
+        [SerializeField] private T desktopData;
+
+        protected bool GetIsMobile()
+        {
+            return SystemInfo.deviceType == DeviceType.Handheld;
+        }
+
+        public T GetPanelData()
+        {
+            bool isMobile = GetIsMobile();
+            string panelType = isMobile ? "Mobile" : "Desktop";
+            Debug.Log($"{panelName} Panel Selected for {panelType}");
+            return isMobile ? mobileData : desktopData;
+        }
+    }
+}
