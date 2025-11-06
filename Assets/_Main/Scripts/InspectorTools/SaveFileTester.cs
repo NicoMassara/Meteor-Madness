4f2ff09570b46f81cf5b7c63037ced63fc4b1042
NicoMassara <nicolasmassara@outlook.com>
2025-11-06 13:06:10 -0300

    SaveManager created and working

---
+++ Assets/_Main/Scripts/InspectorTools/SaveFileTester.cs
@@ -0,0 +1,81 @@
+﻿using _Main.Scripts.Save;
+using UnityEngine;
+using UnityEditor;
+
+namespace _Main.Scripts.InspectorTools
+{
+#if UNITY_EDITOR
+
+    [ExecuteInEditMode]
+    public class SaveFileTester : MonoBehaviour
+    {
+        [SerializeField] private string saveFileName;
+        [SerializeField] private TestSaveData saveData;
+
+        public void Save()
+        {
+            SaveSystem.Save(saveData,saveFileName);
+        }
+        
+        public void Load()
+        {
+            var temp = SaveSystem.LoadSaveFile<TestSaveData>(saveFileName);
+            if(temp == null) return;
+            Debug.Log($"Score: {temp.Score}\n" +
+                      $"HighScore: {temp.HighScore}\n" +
+                      $"PlayerName: {temp.PlayerName}");
+        }
+
+        public void Delete()
+        {
+            SaveSystem.DeleteSaveFile(saveFileName);
+        }
+
+        public void DeleteAll()
+        {
+            SaveSystem.DeleteAllSaves();
+        }
+    }
+
+    [System.Serializable]
+    public class TestSaveData : SaveDataBase
+    {
+        public int Score;
+        public int HighScore;
+        public string PlayerName;
+    }
+    
+#endif
+    
+#if UNITY_EDITOR
+    
+    [CustomEditor(typeof(SaveFileTester))]
+    public class SaveFileTesterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Dibuja el inspector normal
+            DrawDefaultInspector();
+
+            // Agrega el botón
+            SaveFileTester script = (SaveFileTester)target;
+            if (GUILayout.Button("Save"))
+            {
+                script.Save();
+            }
+            if (GUILayout.Button("Load"))
+            {
+                script.Load();
+            }
+            if (GUILayout.Button("Delete"))
+            {
+                script.Delete();
+            }
+            if (GUILayout.Button("Delete All"))
+            {
+                script.DeleteAll();
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/_Main/Scripts/Save/SaveSystem.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace _Main.Scripts.Save
+{
+    public struct SaveParameters
+    {
+        public const string FolderName = "saves";
+        public const string FileExtension = "sav";
+        //
+        public const string SettingsFile = "settings";
+        public const string SaveFile = "data";
+    }
+
+    public static class SaveSystem
+    {
+        
+        private static string GetSavePath(string fileName)
+        {
+            fileName = fileName.Replace("/", "_").Replace("\\", "_");
+            return Path.Combine(GetSaveFolder(), $"{fileName}.{SaveParameters.FileExtension}");
+        }
+
+        private static string GetSaveFolder()
+        {
+            string folderPath = Path.Combine(Application.persistentDataPath, SaveParameters.FolderName);
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                Debug.Log($"Save Folder Created At: {folderPath}");
+            }
+            
+            return folderPath;
+        }
+
+        public static void Save<T>(T data, string fileName = "savefile") where T : SaveDataBase
+        {
+            string path = GetSavePath(fileName);
+            string json = JsonUtility.ToJson(data, true);
+            try
+            {
+                bool isNewSave = !File.Exists(path);
+                
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.Write(json);
+                }
+
+                if (isNewSave)
+                {
+                    Debug.Log($"Save File Created at: {path}");
+                }
+                else
+                {
+                    Debug.Log($"Game saved at: {path}");
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to save game: " + e.Message);
+            }
+        }
+        
+
+        public static T LoadSaveFile<T>(string fileName) where T : SaveDataBase
+        {
+            string path = GetSavePath(fileName);
+            
+            if (!File.Exists(path))
+            {
+                Debug.LogWarning($"No save file found at: {path}");
+                return null;
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string json = reader.ReadToEnd();
+                    T data = JsonUtility.FromJson<T>(json);
+                    return data;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to load game: " + e.Message);
+                return null;
+            }
+        }
+
+        public static void DeleteSaveFile(string fileName)
+        {
+            string path = GetSavePath(fileName);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Debug.Log($"Save File Deleted at: {path}");
+            }
+            else
+            {
+                Debug.LogWarning($"No save file found at: {path}");
+            }
+        }
+
+        public static void DeleteAllSaves()
+        {
+            string folder = GetSaveFolder();
+
+            if (Directory.Exists(folder))
+            {
+                try
+                {
+                    string[] files = Directory.GetFiles(folder);
+                    foreach (var file in files)
+                    {
+                        File.Delete(file);
+                        Debug.Log($"Delete save at: {file}");
+                    }
+                    Debug.Log($"All saves deleted");
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Failed to delete all saves: {e.Message}");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Save Folder Not Found at: {folder}");
+            }
+        }
+
+    }
+
+    [System.Serializable]
+    public abstract class SaveDataBase {}
+}
