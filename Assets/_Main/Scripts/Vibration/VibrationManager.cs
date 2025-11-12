17ea581670d217c01a36f37216787c452082c7da
NicoMassara <nicolasmassara@outlook.com>
2025-11-12 12:25:07 -0300

    Files update
    Added default vibration parameters
    Added VibrationDataSo

---
+++ Assets/_Main/Scripts/Vibration/VibrationManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using _Main.Scripts.MyComponents;
+using UnityEngine;
+
+namespace _Main.Scripts.Vibration
+{
+    public class VibrationManager : SingletonBehaviour<VibrationManager>
+    {
+        private VibrationController _vibrationController;
+        
+        public event Action OnVibrate;
+        public event Action OnStopVibration;
+
+        private void Awake()
+        {
+            _vibrationController = new VibrationController();
+        }
+
+        private void Start()
+        {
+            _vibrationController.OnVibrate += OnVibrate;
+            _vibrationController.OnStopVibration += OnStopVibration;
+        }
+
+        public void Vibrate(VibrationData data)
+        {
+            _vibrationController.Vibrate(data.Duration, data.Intensity);
+        }
+
+        public void Vibrate(VibrationDurationType duration, VibrationIntensityType intensity)
+        {
+            _vibrationController.Vibrate(VibrationTools.GetDuration(duration), VibrationTools.GetIntensity(intensity));
+        }
+
+        public void Vibrate(VibrationType type)
+        {
+            Vibrate(VibrationTools.GetType(type));
+        }
+
+        public void CancelVibration()
+        {
+            _vibrationController.CancelVibration();
+        }
+    }
+
+    [Serializable]
+    public class VibrationData
+    {
+        [Range(10,3000)]
+        public long Duration;
+        [Range(1,255)]
+        public int Intensity;
+    }
+    
+    public enum VibrationDurationType
+    {
+        None,
+        Short,
+        Medium,
+        Long
+    }
+
+    public enum VibrationIntensityType
+    {
+        None,
+        Light,
+        Medium,
+        Heavy
+    }
+    
+    public enum VibrationType
+    {
+        None,
+        Button
+    }
+
+}
---
+++ Assets/_Main/Scripts/Vibration/VibrationTools.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _Main.Scripts.Vibration
+{
+    public class VibrationTools
+    {
+        public static long GetDuration(VibrationDurationType duration)
+        {
+            return duration switch
+            {
+                VibrationDurationType.Short => 100,
+                VibrationDurationType.Medium => 250,
+                VibrationDurationType.Long => 1000,
+                _ => GetDuration(VibrationDurationType.Medium)
+            };
+        }
+        
+        public static int GetIntensity(VibrationIntensityType intensity)
+        {
+
+            return intensity switch
+            {
+                VibrationIntensityType.Light => 75,
+                VibrationIntensityType.Medium => 128,
+                VibrationIntensityType.Heavy => 200,
+                _ => GetIntensity(VibrationIntensityType.Medium)
+            };
+        }
+        
+        public static VibrationData GetType(VibrationType type)
+        {
+            return type switch
+            {
+                VibrationType.Button => new VibrationData
+                {
+                    Duration = GetDuration(VibrationDurationType.Short),
+                    Intensity = GetIntensity(VibrationIntensityType.Light)
+                },
+                _ => GetType(VibrationType.Button)
+            };
+        }
+    }
+}
