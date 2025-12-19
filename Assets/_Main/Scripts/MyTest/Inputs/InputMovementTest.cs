af9f70e51c4451039f3610543ea909baddf29e0e
NicoMassara <nicolasmassara@outlook.com>
2025-12-19 11:05:44 -0300

    New Movement Version 2.1

--- Assets/_Main/Scripts/MyTest/Inputs/InputMovementTest.cs
+++ Assets/_Main/Scripts/MyTest/Inputs/InputMovementTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using _Main.Scripts.DebugGUI;
+using _Main.Scripts.FiniteStateMachine;
 using _Main.Scripts.MyInputs;
 using UnityEngine;
 
@@ -8,94 +11,211 @@ namespace _Main.Scripts.MyTest.Inputs
     {
         [Header("Components")] 
         [SerializeField] private Transform spriteContainer;
-
-        [Serializable]
-        private class MovementData
-        {
-            [Range(1,50)]
-            [SerializeField] private float rotateSpeed = 180f;
-            [Range(1,50)]
-            public float SnapSpeed = 10f;
-            public float GetRotateSpeed() => rotateSpeed * 10;
-        }
-        [SerializeField] private MovementData movementData;
-
-        private class NewShieldMovement
-        {
-            private const int TotalSlots = GameParameters.GameplayValues.AngleSlots;
-            public int Direction { get; set; }
-            private readonly Transform _objectToRotate;
-            private readonly MovementData _movementData;
-            private int _currentSlot = 0;
-            
-            private float _currentAngle;
-            private float _targetAngle;
-            private bool _snapping;
-            
-            public NewShieldMovement(Transform objectToRotate, MovementData movementData)
-            {
-                _objectToRotate = objectToRotate;
-                _movementData = movementData;
-            }
-
-            public void Execute(float deltaTime)
-            {
-                if (Mathf.Abs(Direction) > 0.01f)
-                {
-                    _currentAngle += Direction * GetFinalSpeed() * deltaTime;
-                    _snapping = false;
-                }
-                else if (!_snapping)
-                {
-                    float step = 360f / TotalSlots;
-                    _targetAngle = Mathf.Round(_currentAngle / step) * step;
-                    _snapping = true;
-                }
-
-                if (_snapping)
-                {
-                    _currentAngle = Mathf.LerpAngle(
-                        _currentAngle,
-                        _targetAngle,
-                        deltaTime * _movementData.SnapSpeed
-                    );
-
-                    if (Mathf.Abs(Mathf.DeltaAngle(_currentAngle, _targetAngle)) < 0.1f)
-                    {
-                        _currentAngle = _targetAngle;
-                        _snapping = false;
-                    }
-                }
-                
-                _objectToRotate.rotation = Quaternion.Euler(0f, 0f, _currentAngle);
-            }
-
-            private float GetFinalSpeed()
-            {
-                return _movementData.GetRotateSpeed();
-            }
-        }
         
-        private NewShieldMovement _shieldMovement;
+        [SerializeField] private RotationData rotationData;
+        private IShieldMovement _shieldMovement;
 
 
         private void Awake()
         {
-            _shieldMovement = new NewShieldMovement(spriteContainer, movementData);
+            _shieldMovement = new RotationMovement(rotationData,spriteContainer);
         }
-        
+
         private void Start()
         {
             var inputReader = GetComponent<InputReader>();
             inputReader.OnMovementDirectionChanged += (value) =>
             {
-                _shieldMovement.Direction = value;
+                _shieldMovement.SetDirection(value);
             };
         }
 
         private void Update()
         {
-            _shieldMovement.Execute(Time.deltaTime);
+            _shieldMovement.ExecuteMovement(Time.deltaTime);
         }
     }
+    
+    public interface IShieldMovement
+    {
+        public void SetDirection(float direction);
+        public void ExecuteMovement(float deltaTime);
+
+    }
+
+    [Serializable]
+    public class RotationData
+    {
+        [Range(1,1000)]
+        public float maxAngularSpeed = 100;
+
+        [Tooltip("Degrees per second")]
+        [Range(1, 3600)] 
+        [SerializeField] private float angularAcceleration = 1f;
+        
+        [Tooltip("Degrees per second")]
+        [Range(1,3600)] 
+        [SerializeField] private float angularDeAcceleration = 1f;
+
+        [Range(1, 100)]
+        [SerializeField] private float snapSpeed = 10;
+
+        [Range(1, 25)] 
+        [SerializeField] private float directionResponse  = 25f;
+
+        public float GetAngularAcceleration() => angularAcceleration;
+        public float GetAngularDeAcceleration() => angularDeAcceleration;
+        public float GetSnapSpeed() => snapSpeed;
+
+        public float GetDirectionResponse() => directionResponse;
+    }
+
+    public class RotationMovement : IShieldMovement
+    {
+        private const int SlotCount = GameParameters.GameplayValues.AngleSlots;
+        private readonly RotationData _data;
+        private readonly Transform _objectToRotate;
+        private float _direction;
+        private float _angularSpeed;
+        private float _lastAngularSpeed;
+        private float _snapAngle;
+        private float _lastDirection;
+        private bool _isSnapping;
+        private bool _isStopped;
+        private bool _isRevering;
+
+        public event Action OnStartMoving;
+        public event Action OnStopped;
+        public event Action OnDirectionChange;
+        
+        
+        public RotationMovement(RotationData data, Transform objectToRotate)
+        {
+            _data = data;
+            _objectToRotate = objectToRotate;
+            _isStopped = true;
+        }
+
+        public void SetDirection(float direction)
+        {
+            _direction = direction;
+        }
+
+        public void ExecuteMovement(float deltaTime)
+        {
+            if (_direction != 0)
+            {
+                _isSnapping = false;
+                _isStopped = false;
+
+                if (_lastAngularSpeed == 0)
+                {
+                    Debug.Log("On Start Moving");
+                    OnStartMoving?.Invoke();
+                }
+
+                var isReversing = _angularSpeed != 0 &&
+                                  _direction != 0 &&
+                                  Mathf.Sign(_direction) != Mathf.Sign(_angularSpeed);
+                
+                if (isReversing)
+                {
+                    if (!_isRevering)
+                    {
+                        _isRevering = true;
+                        
+                        Debug.Log("Direction Changed");
+                        OnDirectionChange?.Invoke();
+                    }
+                    
+                    float target = _data.maxAngularSpeed * _direction;
+
+                    var finalAcceleration = _data.GetAngularAcceleration() * _data.GetDirectionResponse();
+                    
+                    _angularSpeed = Mathf.MoveTowards(
+                        _angularSpeed,
+                        target,
+                        finalAcceleration * deltaTime
+                    );
+                }
+                else
+                {
+                    _isRevering = false;
+                    
+                    _angularSpeed += _direction * _data.GetAngularAcceleration() * deltaTime;
+                }
+                
+                _angularSpeed = Mathf.Clamp(_angularSpeed,  
+                    -_data.maxAngularSpeed, _data.maxAngularSpeed);
+                
+                RotateObject(deltaTime);
+            }
+            else
+            {
+                _angularSpeed = Mathf.MoveTowards(_angularSpeed, 
+                    0f, _data.GetAngularDeAcceleration() * deltaTime);
+                
+                RotateObject(deltaTime);
+
+                if (Mathf.Approximately(_angularSpeed, 0f) && !_isSnapping && !_isStopped)
+                {
+                    _isSnapping = true;
+
+                    var slotSize = GetSlotSize();
+                    var currentAngle = GetCurrentAngle();
+
+                    if (_lastDirection > 0)
+                    {
+                        _snapAngle = Mathf.Ceil(currentAngle / slotSize) * slotSize;
+                    }
+                    else
+                    {
+                        _snapAngle = Mathf.Floor(currentAngle / slotSize) * slotSize;
+                    }
+
+                    _snapAngle = (_snapAngle + 360f) % 360f;
+                }
+
+
+                if (_isSnapping)
+                {
+                    var finalSnapSpeed = _data.GetSnapSpeed() * deltaTime;
+                    
+                    _objectToRotate.rotation = Quaternion.Lerp( 
+                        _objectToRotate.rotation, 
+                        Quaternion.Euler(0f,0f, _snapAngle), 
+                        finalSnapSpeed);
+
+
+                    if (Math.Abs(Mathf.DeltaAngle(GetCurrentAngle(), _snapAngle)) < 0.1f)
+                    {
+                        _objectToRotate.rotation = Quaternion.Euler(0f,0f,_snapAngle);
+                        _isSnapping = false;
+                        _isStopped = true;
+                        Debug.Log("Stopped");
+                        OnStopped?.Invoke();
+                    }
+                }
+            }
+
+            _lastDirection = _direction;
+            _lastAngularSpeed = _angularSpeed;
+        }
+
+        private void RotateObject(float deltaTime)
+        {
+            _objectToRotate.Rotate(0f, 0f, _angularSpeed * deltaTime);
+        }
+
+        private float GetCurrentAngle()
+        {
+            return _objectToRotate.localEulerAngles.z;
+        }
+
+        private float GetSlotSize()
+        {
+            return  360f / SlotCount;
+        }
+    }
+
 }
